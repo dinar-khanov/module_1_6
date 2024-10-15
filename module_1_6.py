db1e0f67930d73ab574cf2208099b205b76831ff
dinar-khanov <hanov.di@gmail.com>
2024-10-15 12:04:48 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,9 @@
+name="Python" # курс
+print(name)
+all_tasks=12.0 # Количество_выполненных_ДЗ
+print(all_tasks)
+all_times=1.5 # Количество_затраченных_часов
+print(all_times)
+time_for_1_task=all_times/all_tasks # Время_на_одно_задание
+print(time_for_1_task)
+print(name,',всего задач:',all_tasks, 'затрачено часов:',all_times,'среднее время выполнения:',time_for_1_task)
